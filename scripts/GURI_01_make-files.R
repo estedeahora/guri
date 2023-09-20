7138624aaf3ddf910930d15c750ffe4a1e2334c4
estedeahora_pc <pabloserrati@gmail.com>
2023-09-20 16:07:45 -0300

    Simplificar script de makefiles
    Se arma para generar todos los archivos juntos.

--- scripts/GURI_01_make-files.R
+++ scripts/GURI_01_make-files.R
@@ -3,7 +3,7 @@
 journal <- "QUID16"      # Define directorio de la revista (no usar espacios)
 prefijo <- "num"         # Prefijo para diferenciar número/volumen. Define directorio de número actual
 
-numero <- 30
+numero <- 20
 
 # Definir paths
 path_root    <- paste0("./", journal, "/") 
@@ -22,13 +22,16 @@ library(crayon)
 
 source("scripts/GURI_00_fx.R", encoding = "UTF-8")
 
-# Armado de archivos base
-
-# Obtener archivos de cada artículo
+# Armado de archivos base de cada artículo
 #   * Devuelve lista de archivos en cada carpeta ./{issue}/{art}
 #   * Diferencia artículos según tipo
 art <- GURI_listfiles(path_issue) 
 
-GURI(art$art_path[1], art$art_id[1], verbose = F)
-GURI(art$art_path[2], art$art_id[2], verbose = F)
-GURI(art$art_path[3], art$art_id[3], verbose = F)
+# Armado de archivos finales
+walk2(art$art_path, art$art_id, 
+      \(x, y) GURI(x, y, verbose = F) )
+
+# i <- 6
+# GURI(art$art_path[i], art$art_id[i], verbose = F)
+
+
