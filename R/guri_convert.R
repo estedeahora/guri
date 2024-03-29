6be2d3ead3333cf9fbc0d70894514212afedd821
estedeahora_pc <pabloserrati@gmail.com>
2024-03-28 21:07:38 -0300

    * Add config_path to metadata pandoc field. * Add metadata options (partial). * Add template options (partial). * Fix bibliography options. * Other minor adjustments

--- R/guri_convert.R
+++ R/guri_convert.R
@@ -9,8 +9,16 @@
 
 # path_art <- "./example/num1/art101_lorem-ipsum"
 # art <- "art101"
-# verbose <- F
+# verbose <- T
 # output <- "md"
+
+# TODO
+#   [x] General options
+#   [x] Lua Filters
+#   [x] Appendix (md)
+#   [x] Bibliography (md)
+#   [-] Templates (pdf y html)
+#   [-] Metadata (md y pdf)
 
 guri_convert <- function(path_art, art,
                          output,
@@ -31,17 +39,11 @@ guri_convert <- function(path_art, art,
   config_path  <- file.path(wdir, "..", "..", "_config"  )
   config_files <- list.files(config_path)
 
-  # TODO
-  #   [x] General options
-  #   [x] Lua Filters
-  #   [-] Appendix (md)
-  #   [-] Bibliography (md)
-  #   [ ] Templates (pdf y html)
-  #   [ ] Metadata (md y pdf)
-
   # General options
   opt_gral <- pandoc_options$opts[c("option", output)]
   opt_gral <- opt_gral$option[order(opt_gral$md) & !is.na(opt_gral$md)]
+
+  opt_gral <- c(opt_gral, paste0("--metadata=config_path:", file.path(program_path, "filters")))
 
   # Lua Filters
   opt_filters <- pandoc_options$lua[c("filter", output)]                            # select pandoc options
@@ -54,6 +56,9 @@ guri_convert <- function(path_art, art,
 
   # Customised filters
   customised_filters <- config_files[stringr::str_detect(config_files, paste0(output, "_[0-9]_.+\\.lua$"))]
+  if(verbose & length(customised_filters) > 0){
+    ui_alert_info("Customized lua filters used: ", paste(col_blue(customised_filters), collapse = "; "), ".")
+  }
   customised_filters <- file.path(config_path, sort(customised_filters))
 
   opt_filters <- c(opt_filters, customised_filters)
@@ -61,7 +66,7 @@ guri_convert <- function(path_art, art,
   # Appendix (Only in docx -> md)
   app_files <- NULL
 
-  if(!is.na(opt_type["appendix"])) {
+  if(!is.na(opt_type[["appendix"]])) {
     app_files <- guri_appendix(wdir, art)
     if(length(app_files) > 0){
       if(verbose){
@@ -74,12 +79,15 @@ guri_convert <- function(path_art, art,
   # Bibliography options (Only in docx -> md)
   opt_biblio <- NULL
 
-  if(!is.na(opt_type["biblio"])) {
+  if(!is.na(opt_type[["biblio"]])) {
 
     config_csl <- config_files[stringr::str_detect(config_files, ".*\\.csl$") ]
 
     if(length(config_csl) == 1){
-      op_biblio <- c(op_biblio, paste0("--csl=", config_path, config_csl))
+      opt_biblio <- c(op_biblio, paste0("--csl=", file.path(config_path, config_csl) ))
+      if(verbose){
+        ui_alert_info("Citation style used: {.path ", config_csl, "}.")
+      }
     }else if(length(config_csl) > 1){
       ui_abort("There are multiple 'csl' files in {.path 'JOURNAL/_config/'}. Only one csl file should be provided.")
     }else{
@@ -87,15 +95,50 @@ guri_convert <- function(path_art, art,
     }
   }
 
+  # Templates (Only in md -> tex/html)
+  opt_templ <- NULL
+
+  if(!is.na(opt_type[["template"]])) {
+
+    template_file <- paste0("template.",  opt_type[["template"]])
+
+    config_template <- config_files[config_files ==  template_file]
+
+    if(config_template == "template.latex"){
+      opt_templ <- paste0("--template=", config_path, config_latex_template)
+    }else{
+      if(verbose){
+        ui_alert_info("Default ", opt_type["to"],  " template is used.")
+      }
+      cat("No existe archivo 'root/_config/latex.template'.",
+          "Se usará latex.template por defecto")
+      opt_templ <- paste0("--template=", program_path, "template/", config_latex_template)
+    }
+  }
+
   # Metadata files
-  opt_meta <- c(paste0("--metadata-file=./", art, ".yaml"), # artículo
-                "--metadata-file=../_issue.yaml",           # número
-                "--metadata-file=../../_journal.yaml")      # revista
-  # }else{
-  #   app_files <- NULL
-  #   op_meta <- NULL
-  #   op_biblio <- NULL
-  # }
+  opt_meta <- NULL
+
+  if(output == "md"){
+
+    metadata_files <- c(art = file.path(paste0(art, ".yaml")),              # article
+                        issue = file.path("..", "_issue.yaml"),             # issue
+                        journal = file.path("..", "..", "_journal.yaml"))   # journal
+
+    metadata_exist <- file.exists(file.path(wdir, metadata_files) )
+
+    if(any(!metadata_exist)){
+      ui_abort("Configuration file is not present. Missing: {.path ",
+               metadata_files[!metadata_exist], "}" )
+    }
+
+    opt_meta <- paste0("--metadata-file=", metadata_files)
+
+  }else if(!is.na(opt_type[["metadata"]])){
+
+    # TODO TEX/HTML
+
+  }
 
   rmarkdown::pandoc_convert(wd = wdir,
                             input = file_input,
@@ -109,29 +152,5 @@ guri_convert <- function(path_art, art,
   )
 
 }
-
-
-
-# op_gral
-# ------*
-#
-# docx -> Markdown
-# op_gral <- c( "-s", "--wrap=none", #"--log=./log_pandoc.log",
-#               "--extract-media=./"
-#               )
-#
-# Markdown -> HTML
-# op_gral <- c( "-s", "--wrap=none", "--metadata=link-citations",
-#              "--mathml", "--reference-links=true",
-#              paste0("--template=", program_path, "template/template.html"))
-#
-# Markdown -> JATS
-# op_gral <- c("--wrap=none", "--mathml", "--reference-links=true",
-#              paste0("--metadata-file=", program_path, "pandoc/jats_metadata.yaml"),
-#              paste0("--template=", program_path, "template/template_default.jats_publishing"))
-#
-# Markdown -> tex
-# op_gral <- c( "-s", "--pdf-engine=lualatex" )
-
 
 
