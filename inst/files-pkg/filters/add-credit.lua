567a6184438ab70316bf3d3973d54a7a2e8326b9
estedeahora <pabloserrati@gmail.com>
2024-04-10 11:55:48 -0300

    Modify credit 'cont' according to m.lang
    If: lang = 'en'
    → cont = credit_term = elem
    else cont = columna de xlsx

--- inst/files-pkg/filters/add-credit.lua
+++ inst/files-pkg/filters/add-credit.lua
@@ -1,7 +1,15 @@
---- add-credit.lua – filter to filter to add credit data to author metadata (in art[~]_credit.csv file)
+--- add-credit.lua – filter to add credit data to author metadata (from art[~]_credit.csv file)
 --- https://github.com/estedeahora/guri/tree/main/inst/files-pkg/filters/add-credit.lua
 --- Copyright: © 2024 Pablo Santiago SERRATI
 --- License: CC-by-nc-sa
+
+-- function credit_dict() --------------------------------------------------------------
+-- Description: [en] Creates lists with credit terms and url to credit 
+--				[es] Crea listas con términos credit (en inglés) y url a credit
+-- Return: [en] A list with two lists. The first with the credit terms in English ('credit_term') 
+--					and the second with the urls to each of the credit roles ('credit_uri').
+--		   [es] Una lista con dos listas. La primera con los términos credit en inglés ('credit_term')
+--					 y la segunda con los url a cada uno de los roles credir ('credit_uri').  
 
 local function credit_dict()
 	local credit_term = {"Conceptualization", "Data curation", "Formal Analysis", 
@@ -22,11 +30,18 @@ local function credit_dict()
 	return credit_term, credit_uri
 end
 
--- Meta(m) --------------------------------------------------------------------------------------
+-- Meta(m) ----------------------------------------------------------------------------
 -- Description: [en] Incorporates credit roles (as table) within author 
 --				[es] Incorpora roles credit (como tabla) dentro de author
--- Return: [en] Modified 'Meta' containing a table with credit roles inside meta.author[i].credit. 
---		   [es] Meta modificado conteniendo una tabla con roles credit dentro de meta.author[i].credit. 
+-- Return: [en] Modified 'Meta' containing a table with the credit roles inside meta.author[i].credit
+--					(according to the role it has in the article). Each table contains 'cont' with
+--					the role translated into the language of the article (if it is in English, 'elem' 
+--					is replicated); 'elem' with the translated credit role; and 'uri' with the url to
+--					 the credit role.
+--		   [es] Meta modificado conteniendo una tabla con los roles credit dentro de meta.author[i].credit 
+--					(según el rol que tiene en el artículo). Cada tabla contiene 'cont' con el rol 
+--					traducido al idioma del artículo (si es en inglés se replica 'elem'); 'elem' con
+--					 el rol credit traducido; y 'uri' con la url al rol credit.
 
 function Meta(m)
 
@@ -62,10 +77,21 @@ function Meta(m)
 			credit[i] = {}
 		end
 	
+		-- assigns credit according to roles performed by each author.
 		for i, row in pairs(datos) do
 			for k = 2, n_aut + 1 do
 				if row[k] ~= "" then
-					table.insert(credit[k-1], {cont = row[1], 
+
+					local cont 
+
+					-- if lang == 'en', then cont = elem
+					if pandoc.utils.stringify(m.lang):match('en') then
+						cont = credit_term[i]
+					else
+						cont = row[1]
+					end
+
+					table.insert(credit[k-1], {cont = cont,--row[1],
 											   elem = credit_term[i], 
 											   uri = credit_uri[i]})
 				end
