96d0591b8e17420ec2d8b451e53c1a8e9369281b
estedeahora <pabloserrati@gmail.com>
2024-04-12 15:40:37 -0300

    add doi-builder to pandoc_options

--- inst/files-pkg/filters/doi-builder.lua
+++ inst/files-pkg/filters/doi-builder.lua
@@ -13,6 +13,7 @@ local function Meta(meta)
         meta.article.doi = journal.doi_prefix .. meta.article.doi
     else
         local doi_suffix
+        print()
 
         doi_suffix = doi_register.doi_suffix_constructor:gsub('', '')
         meta.article.doi = journal.doi_prefix .. doi_suffix
@@ -21,3 +22,15 @@ local function Meta(meta)
     
 
 end
+
+
+-- # 'doi_suffix_constructor' follows OJS conventions: 
+-- #    %j journal initials (first letter of journal.title); 
+-- #    %v volume nomber (volume); 
+-- #    %i issue number (issue); 
+-- #    %Y Year (date.year); 
+-- #    %a OJS article ID (article.ojs_id); 
+-- #    %p article pages (article.fpage-article.lpage).
+-- # In addition, guri uses two markers that can be used more freely to customise your doi suffix: 
+-- #    %n article.elocation-id; 
+-- #    %x article.publisher-id. 
