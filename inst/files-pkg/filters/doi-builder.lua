e769a8eb31338f2ad61b91b6f1987dc35d1a80ed
estedeahora <pabloserrati@gmail.com>
2024-04-12 15:18:24 -0300

    doi builder
    Nuevo filtro para generar doi de forma programática.

---
+++ inst/files-pkg/filters/doi-builder.lua
@@ -0,0 +1,23 @@
+--- doi-builder.lua – filter to doi construct from patterns
+--- https://github.com/estedeahora/guri/tree/main/inst/files-pkg/filters/article-metadata.lua
+--- Copyright: © 2024 Pablo Santiago SERRATI
+--- License: CC-by-nc-sa
+
+local stringify = pandoc.utils.stringify
+
+local function Meta(meta) 
+
+    if meta.article.doi == "none" then
+        io.write("NOTE: The article has no DOI.\n")
+    elseif meta.article.doi then
+        meta.article.doi = journal.doi_prefix .. meta.article.doi
+    else
+        local doi_suffix
+
+        doi_suffix = doi_register.doi_suffix_constructor:gsub('', '')
+        meta.article.doi = journal.doi_prefix .. doi_suffix
+        
+    end
+    
+
+end
