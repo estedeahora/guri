68993f39c6002bba58769455cb69cea2d990fb59
estedeahora <pabloserrati@gmail.com>
2024-04-17 15:27:17 -0300

    add language translates
    Se generaliza la raducción para diferentes idiomas.

--- inst/files-pkg/filters/translate-zotero.lua
+++ inst/files-pkg/filters/translate-zotero.lua
@@ -3,27 +3,48 @@
 --- Copyright: © 2024 Pablo Santiago SERRATI
 --- License: CC-by-nc-sa
 
+local dict = {
+    es = {chapter = 'capítulo', paragraph = 'párrafo' },
+    pt = {chapter = 'capítulo', paragraph = 'parágrafo' },
+    fr = {chapter = 'chapitre', paragraph = 'paragraphe' },
+    it = {chapter = 'capitolo', paragraph = 'paragrafi' },
+    de = {chapter = 'kapitel', paragraph = 'paragraphen' },
+    zh = {chapter = '章', paragraph = '段落' }
+}
+
+local stringify = pandoc.utils.stringify
+local lower = pandoc.text.lower
+
+local lang
+
 -- Cite(cite) --------------------------------------------------------------------------------------
 -- Description: [en] Translate citation elements (coming from zotero plug-in for word in 'fields').
 --				[es] Traducir elementos de cita (provenientes de complemento de zotero para word en 'campos').
 -- Return: [en] An element of type pandoc.cite with suffixes translated into Spanish.
 --		   [es] Un elemento de tipo pandoc.cite con sufijos traducidos al español.
 
-local lang = es
+function Meta(meta)
+    lang = lower(stringify(meta.lang):match('^..'))
+end
+
 function Cite(cite)
 
-    if not lang:match('^en') then
+    if not lang:match('^en') and not lang:match('^la') then
 
-        for i = 1, #cite.citations, 1 do
+        for k, citation in ipairs(cite.citations)do
 
-            local c = cite.citations[i].suffix
+            if citation.suffix ~= nil then
 
-            if c ~= nil then
-                if lang:match('^es') or lang:match('^pt')  then
-                    c = stringify(c):gsub("page", "p."):gsub("pages", "pp."):gsub("chapter", "capítulo"):gsub("paragraph", "párrafo")
+                if dict[lang] then
+                    citation.suffix = stringify(citation.suffix):
+                                        gsub("page", "p."):
+                                        gsub("pages", "pp."):
+                                        gsub("chapter", dict[lang].chapter):
+                                        gsub("paragraph", dict[lang].paragraph)
+                else
+                    warn('Partially supported language ("' ..  lang .. '"). No translation for citation suffix label.\n')
                 end
 
-                cite.citations[i].suffix = c
             end
 
         end
