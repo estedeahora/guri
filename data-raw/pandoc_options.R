9a8abe949786f8319557a5295afa15a61e724dc1
estedeahora <pabloserrati@gmail.com>
2024-10-07 23:02:57 -0300

    Actualize to Pandoc 3.3

--- data-raw/pandoc_options.R
+++ data-raw/pandoc_options.R
@@ -6,7 +6,7 @@ sheets <- c("type", "opts", "lua")
 pandoc_options <- lapply(sheets, \(.x) readxl::read_excel(data_excel, sheet = .x))
 names(pandoc_options) <- sheets
 
-pandoc_req <- "3.2"
+pandoc_req <- "3.3"
 guri_version <- "1.0.0"
 
 usethis::use_data(pandoc_options, guri_version, pandoc_req,
